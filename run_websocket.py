ec4806aaaaf19f51605d41f29236d6837e15d3dc
Zhe Cui <zcui@umiacs.umd.edu>
2018-02-14 20:28:45 -0500

    changing to web socket for streaming results

--- run_websocket.py
+++ run_websocket.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from computation_request import computation_request
-from flask_socketio import SocketIO
+from flask_cache import Cache
+from flask_socketio import SocketIO, emit, send
+
 import json
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-# app.config['host'] = 'localhost'
-# app.config['port'] = 5001
+cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 0})
 
 socketio = SocketIO(app)
 
 
-@socketio.on('json')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
+@socketio.on('get_data_event', namespace='/getdata')
+@cache.memoize()
+def feed(message):
 
+    print "#$$$####################################################"
 
 # @app.route('/chr=<chromosome>&start=<start>&end=<end>', methods=['GET', 'OPTIONS'])
 # def feed(start, end, chromosome):
@@ -109,62 +112,7 @@ def handle_my_custom_event(json):
 #     return measurements
 #
 #
-# # road map measurements
-# def roadmap_measurements(expression=True, block=True, methylation=True):
-#     measurements = []
-#     gene_types = ['breast___normal', 'breast___tumor', 'colon___normal',
-#                   'colon___tumor', 'lung___normal', 'lung___tumor',
-#                   'thyroid___normal', 'thyroid___tumor']
-#
-#     tissue_types = ['breast', 'colon', 'thyroid', 'lung']
-#     if expression:
-#         for gene_type in gene_types:
-#             measurements.append({
-#                 "id": gene_type,
-#                 "name": gene_type,
-#                 "type": "feature",
-#                 "datasourceId": "gene_expression_barcode_subtype",
-#                 "datasourceGroup": "gene_expression_barcode_subtype",
-#                 "dataprovider": "umd",
-#                 "formula": None,
-#                 "defaultChartType": "scatterplot",
-#                 "annotation": None,
-#                 "metadata": ["probe"]
-#             })
-#
-#     if block:
-#         for tissue_type in tissue_types:
-#             measurements.append({
-#                 "id": 'timp2014_' + tissue_type + '_blocks',
-#                 "name": 'timp2014_' + tissue_type + '_blocks',
-#                 "type": "feature",
-#                 "datasourceId": 'timp2014_' + tissue_type + '_blocks',
-#                 "datasourceGroup": 'timp2014_' + tissue_type + '_blocks',
-#                 "dataprovider": "umd",
-#                 "formula": None,
-#                 "defaultChartType": "block",
-#                 "annotation": None,
-#                 "metadata": ["probe"]
-#             })
-#
-#     if methylation:
-#         for tissue_type in tissue_types:
-#             measurements.append({
-#                 "id": tissue_type,
-#                 "name": tissue_type,
-#                 "type": "feature",
-#                 "datasourceId": 'timp2014_collapsed_diff',
-#                 "datasourceGroup": 'timp2014_collapsed_diff',
-#                 "dataprovider": "umd",
-#                 "formula": None,
-#                 "defaultChartType": "line",
-#                 "annotation": None,
-#                 "metadata": ["probe"]
-#             })
-#
-#     return measurements
 
 if __name__ == '__main__':
-    print 'Server Starts!'
-    socketio.run(app, host='localhost', port=5001)
+    socketio.run(app, debug=True, port=5001)
 
