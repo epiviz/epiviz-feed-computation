84db794589f2c03d7d3fd05e2663326f345d0180
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2019-10-25 09:35:20 -0400

    update usecase

--- usecases/epiviz-instance/epivizfeed.py
+++ usecases/epiviz-instance/epivizfeed.py
@@ -3,6 +3,7 @@ import os
 
 if __name__ == "__main__":
 
-    app = setup_app(os.getcwd() + "/epiviz.json")
-    app.start_app()
+    app = setup_app(server="http://54.157.53.251/api/", file=os.getcwd() + "/epiviz.json")
+    # app.start_app()
+    start_app()
     
